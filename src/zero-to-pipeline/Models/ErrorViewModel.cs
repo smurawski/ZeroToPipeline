c640ab5f396e4953ab8bd4d253cda613e788ae7a
yo team <yo team>
2019-11-03 19:09:37 -0500

    Init

---
+++ src/zero-to-pipeline/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace zero-to-pipeline.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
